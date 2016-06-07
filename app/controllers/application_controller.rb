01adc749f5eb307b508d82a6af0ffc0d5a8c54d3
hvijayganesh <h.vijayganesh@gmail.com>
2016-06-07 13:59:25 +0530

    bug fix in application_controller.rb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,5 +4,6 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   def hello
-  	render text: "hello, world !!!"
+    render text: "hello, world!"
+  end
 end
